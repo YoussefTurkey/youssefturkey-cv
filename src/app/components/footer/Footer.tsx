19d869087f370cab7ff31d3fa4d7c2c6ad70ab5b
Youssef Turkey <youssef.turkey11@gmail.com>
2025-03-22 20:30:09 +0200

    changing footer font-size

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
       <footer className='flex justify-center items-center py-5 lg:pt-0 lg:pb-5 border-t-2 border-t-[#212121] lg:border-t-0 w-[100%]'>
-        <p className='text-[#ccc]'>© 2025 YouTurkey11. All Rights Reserved.</p>
+        <p className='text-[#ccc] text-[12px] lg:text-[16px]'>© 2025 YouTurkey11. All Rights Reserved.</p>
       </footer>
     </>
   )
