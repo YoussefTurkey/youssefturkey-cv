4a0f8cda24af58494314b3d36ecc5ca45626eeb8
YoussefTurkey <youssef.turkey11@gmail.com>
2025-05-08 00:28:17 +0300

    adding palestine support flag

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,5 +1,8 @@
-'use client'
+"use client";
 import React from "react";
+import dynamic from "next/dynamic";
+// importing Components
+const Flag = dynamic(() => import("../../components/ads/Flag"));
 // using Translation
 import { useLanguage } from "@/app/lang/LanguageProvider";
 
@@ -9,11 +12,12 @@ const Footer = () => {
   return (
     <>
       <footer className="hidden lg:flex justify-center items-center py-5 lg:pt-0 lg:pb-5 border-t-2 border-t-[#212121] lg:border-t-0 w-[100%]">
-        <p className="text-[12px] lg:text-[16px]">
+        <p className="text-[12px] lg:text-[16px] mx-2">
           {language === "en"
             ? "© 2025 YouTurkey11. All Rights Reserved."
             : "© 2025 YouTurkey11. جميع الحقوق محفوظة."}
         </p>
+        <Flag />
       </footer>
     </>
   );
