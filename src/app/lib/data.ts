1707b7bb9fb2026e8b336acfc942d6630df6ab46
YoussefTurkey <youssef.turkey11@gmail.com>
2025-05-07 22:47:12 +0300

    responsive

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -199,7 +199,7 @@ export const skills: ISkills = {
     ],
   },
   backend: {
-    title: "Backend",
+    title: "Backend & CMS",
     tools: [
       "PHP & Python & C & C++",
       "Strapi & WordPress",
